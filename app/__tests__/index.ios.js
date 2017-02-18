44335318593b66a6c76df6fd9f8d7745689547a0
andywalzdesign <andywalzdesign@gmail.com>
2017-02-17 16:19:44 -0800

    Remove default tests to allow example tests to run

--- app/__tests__/index.ios.js
+++ app/__tests__/index.ios.js
@@ -1,12 +1,21 @@
 import 'react-native';
 import React from 'react';
-import Index from '../index.ios.js';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+var sum = function(a, b) {
+  return a + b;
+}
 
-it('renders correctly', () => {
-  const tree = renderer.create(
-    <Index />
-  );
+test('adds 1 + 2 to get 3', () => {
+  expect(sum(1, 2)).toBe(3);
 });
+
+// import Index from '../index.ios.js';
+
+// // Note: test renderer must be required after react-native.
+// import renderer from 'react-test-renderer';
+
+// it('renders correctly', () => {
+//   const tree = renderer.create(
+//     <Index />
+//   );
+// });
