cd9a283aa6c31759086721dc32b4939b11ffe4b1
andywalzdesign <andywalzdesign@gmail.com>
2017-02-21 16:47:02 -0800

    Add users route test

--- tests/server-tests.js
+++ tests/server-tests.js
@@ -1,9 +1,21 @@
 var chai = require('chai');
+var expect = require('chai').expect;
+var request = require('request');
 
 //
 // TESTS
 //
-
+describe('routes', () => {
+  it('should have a users route', (done) => {
+    request.get('http://localhost:2300/users', function(err, res, body){
+      if(err){
+        console.log("ERROR", err);
+      }
+      expect(res.body).to.exist;
+      done();
+    });
+  });
+});
 //
 // HELPER FUNCTIONS
 //
