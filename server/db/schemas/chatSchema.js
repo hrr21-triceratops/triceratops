61b8675ce69894f44c64acee9a681b638ee6618e
Alex Autem <autem.alex@gmail.com>
2017-03-02 00:29:02 -0500

    Update chat schema to reflect gifted chat message objects

--- server/db/schemas/chatSchema.js
+++ server/db/schemas/chatSchema.js
@@ -1,13 +1,14 @@
 var mongoose = require('../connections.js').mongoose;
 
 var chatSchema = mongoose.Schema({
-  chatSessionID: { type: String, required: true },
-  senderID: { type: Number, required: true },
-  receiverID: { type: Number, required: true },
-  message: { type: String, required: true },
-  date: { type: Date, default: Date.now }
-}, {
-    timestamps: true
+  _id: { type: Number, required: true },
+  text: { type: String },
+  createdAt: { type: Date, default: Date.now },
+  user: {
+    _id: { type: Number, required: true },
+    name: { type: String, required: true }
+  },
+  chatSessionID: { type: String, required: true }
 });
 
 var chatModel = mongoose.model('chatSchema', chatSchema);
