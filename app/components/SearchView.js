b06c027734541f8192501f1a781c6c3f9c150e56
andywalzdesign <andywalzdesign@gmail.com>
2017-03-02 16:38:32 -0800

    Update SearchView to display expert view

--- app/components/SearchView.js
+++ app/components/SearchView.js
@@ -17,12 +17,15 @@ import Tabs from 'react-native-tabs';
 import { SearchBar } from 'react-native-elements'
 
 const usersToHelp = null;
+const usersToCheck = null;
 
 export default class SearchView extends Component {
   constructor(props){
     super(props);
     this.state = {
-      page:'Search'
+      page:'Search',
+      currentUser: null,
+      index: 0
     };
   }
 
@@ -45,10 +48,40 @@ export default class SearchView extends Component {
 
   activeSwitcher() {
     this.setState({isActive: !this.state.isActive});
+    this.renderExpert();
   }
 
   getActive() {
     return this.state.isActive;
+  }
+
+  showNext() {
+    if(usersToCheck.length - 1 > this.state.index) {
+      this.setState({currentUser: usersToHelp[usersToCheck[this.state.index+1]], index: this.state.index+1});
+    } else {
+      this.setState({currentUser: usersToHelp[usersToCheck[0]], index: 0});
+    }
+  }
+
+  renderExpert() {
+    fetch('https://savvyshopper.herokuapp.com/api/userQueue/loadUser', {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }).then((response) => {
+      return response.json();
+      console.log("RESPONSE", response);
+    }).then((users) => {
+      if(!users){
+        console.log("TROUBLE");
+      }
+      console.log("USERS IN QUEUE", users);
+      usersToHelp = users;
+      usersToCheck = Object.keys(usersToHelp);
+      this.setState({currentUser: usersToHelp[usersToCheck[0]]});
+    }).done()
   }
 
   render() {
@@ -106,39 +139,20 @@ export default class SearchView extends Component {
         {this.props.shopperExpert && button}
         {this.getActive() &&
           <View>
-            {fetch('https://savvyshopper.herokuapp.com/api/userQueue/loadUser', {
-              method: 'GET',
-              headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-              }
-            }).then((response) => {
-              response.json();
-              console.log("REPSONE", response);
-            }).then((users) => {
-              if(!users){
-                console.log("TROUBLE");
-              }
-              console.log("USERS IN QUEUE", users);
-              usersToHelp = users;
-              // this.renderExpert();
-            }).done()}
-            <View>
-              <Text>USER</Text>
-              <Text>{JSON.stringify(usersToHelp)}</Text>
-              <TouchableHighlight
-              style={styles.button}
-              onPress={() => this.navigateTo('Chat', this.props)}>
-                <Text style={styles.buttonText}>b</Text>
-              </TouchableHighlight>
-              <TouchableHighlight
-              style={styles.button}
-              onPress={() => this.showNext()}>
-                <Text style={styles.buttonText}>p</Text>
-              </TouchableHighlight>
-            </View>
+            <Text>USER</Text>
+            <Text>{JSON.stringify(this.state.currentUser)}</Text>
+            <TouchableHighlight
+            style={styles.button}
+            onPress={() => this.navigateTo('Chat', this.props)}>
+              <Text style={styles.buttonText}>b</Text>
+            </TouchableHighlight>
+            <TouchableHighlight
+            style={styles.button}
+            onPress={() => this.showNext()}>
+              <Text style={styles.buttonText}>p</Text>
+            </TouchableHighlight>
           </View>
-      }
+        }
       </View>
     );
   }
