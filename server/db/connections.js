89ba78debc5411671dc63a9c9beeae379e9f97aa
Tony Hyde <tonyhyde6@gmail.com>
2017-03-07 10:11:51 -0800

    add search framework

--- server/db/connections.js
+++ server/db/connections.js
@@ -37,9 +37,26 @@ if (process.env.DATABASE_URL) {
 
 }
 
+var elasticsearch = require('elasticsearch');
+var client = elasticsearch.Client({
+  host: 'localhost:9200',
+  log: "info"
+});
+
+client.ping({
+  requestTimeout: 1000
+}, function (error) {
+  if (error) {
+    console.trace('elasticsearch cluster is down!');
+  } else {
+    console.log('All is well');
+  }
+});
+
 module.exports = {
   'sequelize': sequelize,
-  'mongoose': mongoose.connect(mongooseConnectionURL)
+  'mongoose': mongoose.connect(mongooseConnectionURL),
+  'elasticClient': client
 };
 
 var db = mongoose.connection;
---
+++ server/db/schemas/tagSchema.js
@@ -0,0 +1,92 @@
+var elasticClient = require('../connections.js').elasticClient;
+var indexName = "randomindex";
+
+/**
+* Delete an existing index
+*/
+function deleteIndex() {
+    return elasticClient.indices.delete({
+        index: indexName
+    });
+}
+exports.deleteIndex = deleteIndex;
+
+/**
+* create the index
+*/
+function initIndex() {
+    return elasticClient.indices.create({
+        index: indexName
+    });
+}
+exports.initIndex = initIndex;
+
+/**
+* check if the index exists
+*/
+function indexExists() {
+    return elasticClient.indices.exists({
+        index: indexName
+    });
+}
+
+function initMapping() {
+    return elasticClient.indices.putMapping({
+        index: indexName,
+        type: "document",
+        body: {
+            properties: {
+                title: { type: "string" },
+                content: { type: "string" },
+                suggest: {
+                    type: "completion",
+                    analyzer: "simple",
+                    search_analyzer: "simple",
+                    payloads: true
+                }
+            }
+        }
+    });
+}
+
+function addDocument(document) {
+    return elasticClient.index({
+        index: indexName,
+        type: "document",
+        body: {
+            title: document.title,
+            content: document.content,
+            suggest: {
+                input: document.title.split(" "),
+                output: document.title,
+                payload: document.metadata || {}
+            }
+        }
+    });
+}
+
+function getSuggestions(input) {
+    return elasticClient.suggest({
+        index: indexName,
+        type: "document",
+        body: {
+            docsuggest: {
+                text: input,
+                completion: {
+                    field: "suggest",
+                    fuzzy: true
+                }
+            }
+        }
+    });
+}
+
+function searchSuggestions(input) {
+    elasticClient.search({
+      q: input
+    }).then(function (body) {
+      var hits = body.hits.hits;
+    }, function (error) {
+      console.trace(error.message);
+    });
+}
